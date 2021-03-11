d0e7259dcd941813984c1eee71450c3982477f25
Marisa Li <marisa.susan.li@gmail.com>
2021-03-10 21:17:00 -0500

    refactor: updates to banner and about components

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,64 +1,39 @@
-import React, { Component } from "react";
-import Img from "gatsby-image";
+import React, { Component } from 'react';
+import Img from 'gatsby-image';
 
 export default class Banner extends Component {
   render() {
     const { data } = this.props;
     return (
-      <div className="banner">
+      <div className='banner'>
         <Img
           fluid={data.bannerImage.fluid}
-          objectFit="cover"
-          objectPosition="50% 50%"
+          objectFit='cover'
+          objectPosition='50% 50%'
         />
-        <div className="container">
-          <div className="banner-details">
-            <span>Hello...</span>
-            <h1>I'm {data.designation}.</h1>
-            <ul className="sub-data">
+        <div className='container'>
+          <div className='banner-details'>
+            <h1>Marisa Li</h1>
+            <ul className='sub-data'>
               {data.bannerList.map((item, index) => {
                 return <li key={index}>{item}</li>;
               })}
             </ul>
-            <ul className="social">
+            <ul className='social'>
               <li>
                 <a
-                  className="fab fa-facebook-f"
-                  href={data.facebook}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                ></a>
-              </li>
-              <li>
-                <a
-                  className="fab fa-twitter"
-                  href={data.twitter}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                ></a>
-              </li>
-              <li>
-                <a
-                  className="fab fa-instagram"
-                  href={data.instagram}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                ></a>
-              </li>
-              <li>
-                <a
-                  className="fab fa-linkedin-in"
+                  className='fab fa-linkedin-in'
                   href={data.linkdin}
-                  target="_blank"
-                  rel="noopener noreferrer"
+                  target='_blank'
+                  rel='noopener noreferrer'
                 ></a>
               </li>
               <li>
                 <a
-                  className="fab fa-github"
+                  className='fab fa-github'
                   href={data.github}
-                  target="_blank"
-                  rel="noopener noreferrer"
+                  target='_blank'
+                  rel='noopener noreferrer'
                 ></a>
               </li>
             </ul>
