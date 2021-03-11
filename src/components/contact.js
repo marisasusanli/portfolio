8f41c29003e97cae9d581df1e9995fac1f8e77c4
Marisa Li <marisa.susan.li@gmail.com>
2021-03-11 17:08:25 -0500

    feat: configure contact form to send messages

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,36 +1,36 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 export default class Contact extends Component {
   render() {
     return (
-      <div className="contact section" id="Contact">
-        <div className="container">
-          <div className="section-head">
-            <h2 className="text-center">Contact</h2>
+      <div className='contact section' id='Contact'>
+        <div className='container'>
+          <div className='section-head'>
+            <h2 className='text-center'>Contact</h2>
           </div>
           <form
-            action={`https://formspree.io/${this.props.data}`}
-            name="contact"
-            method="POST"
-            data-netlify="true"
+            action={`https://formspree.io/f/xnqoevwb`}
+            name='contact'
+            method='POST'
+            data-netlify='true'
           >
             <div>
               <label>
-                Your Name: <input type="text" name="name" required />
+                Your Name: <input type='text' name='name' required />
               </label>
             </div>
             <div>
               <label>
-                Your Email: <input type="email" name="email" required />
+                Your Email: <input type='email' name='email' required />
               </label>
             </div>
             <div>
               <label>
-                Message: <textarea name="message" required></textarea>
+                Message: <textarea name='message' required></textarea>
               </label>
             </div>
             <div>
-              <button type="submit">Send</button>
+              <button type='submit'>Send</button>
             </div>
           </form>
         </div>
